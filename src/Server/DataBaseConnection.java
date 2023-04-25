5aa9cf11b59c46680c54dd6afce87af0a03c6799
apurveh <apurvamishra573@gmail.com>
2023-04-25 20:43:42 +0200

    Database
    Added driver

---
+++ src/Server/DataBaseConnection.java
@@ -0,0 +1,11 @@
+package Server;
+
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.registerDriver;
+
+public class DataBaseConnection{}
