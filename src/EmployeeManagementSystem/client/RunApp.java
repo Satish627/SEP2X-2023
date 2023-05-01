9ecc5c1a43229151f57f717868802ca00fce8e68
manishagrg04 <monisagrg04@gmail.com>
2023-05-01 18:30:11 +0200

    Employee list table name changed

--- src/EmployeeManagementSystem/client/RunApp.java
+++ src/EmployeeManagementSystem/client/RunApp.java
@@ -2,7 +2,8 @@ package EmployeeManagementSystem.client;
 
 import javafx.application.Application;
 
-public class RunApp {
+public class RunApp
+{
     public static void main(String[] args) {
         Application.launch(App.class);
     }
